a7b461e552d2920635f2a66ad37f2c38a72e4977
wugology <onna.nelson@gmail.com>
2017-06-06 14:27:34 -0700

    Made data frame
    Wrangled JSON data into the format I want to work with.

--- open311nlp.R
+++ open311nlp.R
@@ -1,11 +1,39 @@
 require(RJSONIO)
 
-setwd("~/GitHub/Code for SJ")
+setwd("~/GitHub/Code for SJ/Code-for-SJ")
 requests <- fromJSON('open311.json')
 head(requests, n=3)
+length(requests)
 
+requests[[1]]$service_name
 
-
+services <- vector()
 for(i in 1:length(requests)){
-      
+   if (!is.null(requests[[i]]$description)){
+      temp <- requests[[i]]$service_name
+      services <- append(temp, services)
+      }
    }
+head(services)
+
+requests[[1]]$description
+requests[[2]]$description
+!is.null(requests[[1]]$description)
+!is.null(requests[[2]]$description)
+
+
+description <- vector()
+for(i in 1:length(requests)){
+   temp <- "..."
+   temp <- requests[[i]]$description
+   description <- append(temp, description)
+}
+head(description)
+length(description)
+#notice that 5851 have descriptions; 4149 are null. Add if-clause to services loop to check for nulls.
+
+open311 <- data.frame(services,description)
+head(open311)
+summary(open311)
+unique(open311$services)
+
